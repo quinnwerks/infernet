dbd81565b09b5604936aab5db99a43b24a0d6a27
Quinn Smith <quinn.smith98@gmail.com>
2021-02-03 16:12:36 -0500

    switch to upper case for external signals

--- accelerator/rtl/ip_packet_tx.sv
+++ accelerator/rtl/ip_packet_tx.sv
@@ -8,22 +8,22 @@ module ip_packet_tx (
     // Global signals
     input        aclk,
     input        areset,
-    input [31:0] accelerator_ip_address,
-    input [47:0] accelerator_mac_address,
+    input [31:0] ACCELERATOR_IP_ADDRESS,
+    input [47:0] ACCELERATOR_MAC_ADDRESS,
 
     // Signals interfacing with accelerator
-    input [31:0] recipient_ip_address,
-    input [47:0] recipient_mac_address,
-    input [9:0]  recipient_message, // Either a response to LB or an inference result
-    input        start_ip_txn,
-    output       ready_for_send,
+    input [31:0] RECIPIENT_IP_ADDRESS,
+    input [47:0] RECIPIENT_MAC_ADDRESS,
+    input [9:0]  RECIPIENT_MESSAGE, // Either a response to LB or an inference result
+    input        START_IP_TXN,
+    output       READY_FOR_SEND,
 
     // Signals interfacing with MAC
-    output [7:0] mac_data_out,
-    input        mac_data_ready,
-    output       mac_data_valid,
-    output       mac_data_last,
-    output       mac_data_first
+    output [7:0] MAC_DATA_OUT,
+    input        MAC_DATA_READY,
+    output       MAC_DATA_VALID,
+    output       MAC_DATA_LAST,
+    output       MAC_DATA_FIRST
 );
 
     parameter AXI_S_DATA_WIDTH = 8;
@@ -34,7 +34,6 @@ module ip_packet_tx (
     // List of FSM states
     enum int unsigned {
         IDLE,          // No transation IPR
-        WAIT_MAC,      // Waiting for MAC to be ready
         SEND_ETH_HDR,  // Send ethernet header
         SEND_IP_HDR,   // Send ip header
         SEND_USER_DATA // Send user data
@@ -51,41 +50,53 @@ module ip_packet_tx (
     end
 
     // Internal signal logic
-    logic [IP_ADDR_WIDTH-1:0]    recipient_ip_address_internal;
-    logic [MAC_ADDR_WIDTH-1:0]   recipient_mac_address_internal;
-    logic [ACCEL_DATA_WIDTH-1:0] recipient_message_internal;
-    logic                        start_ip_txn_internal;
-    logic                        ready_for_send_internal;
-    logic [AXI_S_DATA_WIDTH-1:0] mac_data_out_internal;
-    logic                        mac_data_ready_internal;
-    logic                        mac_data_valid_internal;
-    logic                        mac_data_last_internal;
-    logic                        mac_data_first_internal;
+    logic [IP_ADDR_WIDTH-1:0]    recipient_ip_address;
+    logic [MAC_ADDR_WIDTH-1:0]   recipient_mac_address;
+    logic [ACCEL_DATA_WIDTH-1:0] recipient_message;
+    logic                        start_ip_txn;
+    logic                        ready_for_send;
+    logic [AXI_S_DATA_WIDTH-1:0] mac_data_out;
+    logic                        mac_data_ready;
+    logic                        mac_data_valid;
+    logic                        mac_data_last;
+    logic                        mac_data_first;
 
     always_comb begin
-        ready_for_send_internal = 'd0;
+        ready_for_send = 'd0;
 
-        mac_data_out_internal = 'd0;
-        mac_data_valid_internal = 'd0;
-        mac_data_last_internal = 'd0;
-        mac_data_first_internal = 'd0;
+        mac_data_out = 'd0;
+        mac_data_valid = 'd0;
+        mac_data_last = 'd0;
+        mac_data_first = 'd0;
 
     // TODO: impl logic :-)
-    case(state)   
+    case(state)
         IDLE: begin
-            nextstate = state;
+            ready_for_send = mac_data_ready;
+
+            if (start_ip_txn
+             && ready_for_send) begin
+                 nextstate = SEND_ETH_HDR;
+            end
+        end
+        SEND_ETH_HDR: begin
+            ready_for_send = 'd0;
+            if (mac_data_ready == 'd1) begin
+                mac_data_valid = 'd1;
+            end
         end
     endcase
     end
 
     // Internal to external
-    assign recipient_message_internal     = recipient_message;
-    assign recipient_mac_address_internal = recipient_mac_address;
-    assign start_ip_txn_internal          = start_ip_txn;
-    assign ready_for_send                 = ready_for_send_internal;
-    assign mac_data_out                   = mac_data_out_internal;
-    assign mac_data_ready_internal        = mac_data_ready;
-    assign mac_data_last                  = mac_data_last_internal;
-    assign mac_data_first                 = mac_data_first;
+    assign recipient_ip_address  = RECIPIENT_IP_ADDRESS;
+    assign recipient_message     = RECIPIENT_MESSAGE;
+    assign recipient_mac_address = RECIPIENT_MAC_ADDRESS;
+    assign start_ip_txn          = START_IP_TXN;
+    assign READY_FOR_SEND        = ready_for_send;
+    assign MAC_DATA_OUT          = mac_data_out;
+    assign mac_data_ready        = MAC_DATA_READY;
+    assign MAC_DATA_LAST         = mac_data_last;
+    assign MAC_DATA_FIRST        = mac_data_first;
 
 endmodule
