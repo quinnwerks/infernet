10ac75ac61a9237b115c7e6e21fb100d38baf57c
James Liu <james.zhi.liu@gmail.com>
2021-03-12 22:14:43 -0500

    client function to scan for LB using thread pool

--- python/temp_lb_tester.py
+++ python/temp_lb_tester.py
@@ -6,8 +6,11 @@ HOST = sys.argv[1]  # The server's hostname or IP address
 PORT = n532.LB_TCP_PORT        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(n532.CLIENT_DONE_STR + b'1.1.2.2')
-    data = s.recv(1024)
-
-print('Received', repr(data))
+    s.settimeout(3)
+    try:
+        s.connect((HOST, PORT))
+        s.sendall(n532.CLIENT_DONE_STR + b'1.1.2.2')
+        data = s.recv(1024)
+        print('Received', repr(data))
+    except socket.timeout as e:
+        print(e)
